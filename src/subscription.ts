f7449a194d894d933db8f57f255001b24d3c7c3d
mrlctate <1757089@qq.com>
2025-04-17 11:01:52 +0800

    feat: 小米浏览器-分段广告-小说阅读页面广告

--- src/subscription.ts
+++ src/subscription.ts
@@ -19,10 +19,10 @@ apps.forEach((appConfig) => {
 });
 
 export default defineGkdSubscription({
-  // id: 2,
-  id: 9,
-  // name: 'Mrlc的订阅-不得转发和传播',
-  name: 'Mrlc的订阅-Beta',
+  id: 2,
+  // id: 9,
+  name: 'Mrlc的订阅-不得转发和传播',
+  // name: 'Mrlc的订阅-Beta',
   version: 0,
   author: 'Mrlc',
   checkUpdateUrl: './gkd.version.json5',
