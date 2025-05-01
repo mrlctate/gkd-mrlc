99466f973f9a0689ba163a4b98cf3d095eb862ee
mrlctate <1757089@qq.com>
2025-05-01 16:26:28 +0800

    feat: 全局开屏广告 (#补充)

--- src/globalGroups.ts
+++ src/globalGroups.ts
@@ -87,7 +87,7 @@ export default defineGkdGlobalGroups([
       {
         key: 0,
         matches: [
-          `[text*="通知" || text*="权限" || text*="公告" || text="广告" || "text*="消息" || "text*="发现" || "text*="剪贴板" || text="去开启 || text="去设置" || text*="使用" || text*="代理" || text*="定位" || text*="位置" || text*="内容" || text*="分享" || text*="赞助" || text$="提示" || desc$="提示" || desc*="下载" || desc*="官网" || text*="交流" || text*="链接" || text*="关注" || desc*="谢谢"]${CommonPrefix}`,
+          `[text*="通知" || text*="权限" || text*="公告" || text="广告" || text*="消息" || text*="发现" || text*="剪贴板" || text="去开启" || text="去设置" || text*="使用" || text*="代理" || text*="定位" || text*="位置" || text*="内容" || text*="分享" || text*="赞助" || text$="提示" || desc$="提示" || desc*="下载" || desc*="官网" || text*="交流" || text*="链接" || text*="关注" || desc*="谢谢"]${CommonPrefix}`,
           `[text^="暂不" || text$="知道了" || text$="继续使用" || desc$="知道了" || text^="我已知晓" || text="拒绝" || text^="不再" || text$="再说" || text="不允许" || text*="不了" || text="取消" || text="确定" || text="收到" || text="隐藏" || text="否" || id*="close" || id$="Close" || id*="cancel"]${CommonPrefix}`,
         ],
       },
